dbb7543566a57825ff6f3b7866d3437df48e47b1
Alexander Soto <alex@sancocholabs.tech>
2020-10-27 10:48:08 -0400

    migrate codebase to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
-import SearchParams from "./SearchParams";
 import Details from "./Details";
+import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const themeHook = useState("pink");
+  const theme = useState("darkblue");
   return (
-    <ThemeContext.Provider value={themeHook}>
+    <ThemeContext.Provider value={theme}>
       <div>
         <header>
-          <Link to="/"> Adopt Me!</Link>
+          <Link to="/">Adopt Me!</Link>
         </header>
+        ;
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
--- src/SearchParams.tsx
+++ src/SearchParams.tsx
@@ -1,16 +1,22 @@
-import React, { useState, useEffect, useContext } from "react";
-import pet, { ANIMALS } from "@frontendmasters/pet";
-import Results from "./Results";
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  FunctionComponent,
+} from "react";
+import { RouteComponentProps } from "@reach/router";
+import pet, { ANIMALS, Animal } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 import ThemeContext from "./ThemeContext";
 
-const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
-  const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
-  const [pets, setPets] = useState([]);
+const SearchParams: FunctionComponent<RouteComponentProps> = () => {
   const [theme, setTheme] = useContext(ThemeContext);
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([] as string[]);
+  const [pets, setPets] = useState([] as Animal[]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -19,18 +25,20 @@ const SearchParams = () => {
       type: animal,
     });
 
+    console.log("animals", animals);
+
     setPets(animals || []);
   }
 
   useEffect(() => {
-    setBreeds([]);
-    setBreed("");
+    updateBreeds([]);
+    updateBreed("");
 
     pet.breeds(animal).then(({ breeds }) => {
       const breedStrings = breeds.map(({ name }) => name);
-      setBreeds(breedStrings);
+      updateBreeds(breedStrings);
     }, console.error);
-  }, [animal, setBreed, setBreeds]);
+  }, [animal]);
 
   return (
     <div className="search-params">
@@ -46,25 +54,25 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
-            onChange={(event) => setLocation(event.target.value)}
+            onChange={(e) => updateLocation(e.target.value)}
           />
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-        <button style={{ backgroundColor: theme }}>Submit</button>
-        <label htmlFor="theme">
+        <label htmlFor="location">
           Theme
           <select
             value={theme}
             onChange={(e) => setTheme(e.target.value)}
             onBlur={(e) => setTheme(e.target.value)}
           >
-            <option value="pink"> Pink</option>
-            <option value="red"> Red</option>
-            <option value="blue"> Blue</option>
-            <option value="green"> Green</option>
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
